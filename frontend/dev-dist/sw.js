932afe76d1900414864d127944598c52fee1e750
DavidBobet <david.bobet35@gmail.com>
2025-05-07 16:39:44 +0200

    reset img

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.irn21up5r1"
+    "revision": "0.3cg82a0g248"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
